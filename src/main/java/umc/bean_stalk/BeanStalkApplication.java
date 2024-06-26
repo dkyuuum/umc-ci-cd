6e1eb50ba104376862af77a3b02e68b36ac888f7
dkyuuum <dkyuuum@gmail.com>
2024-06-26 15:46:42 +0900

    set up

---
+++ src/main/java/umc/bean_stalk/BeanStalkApplication.java
@@ -0,0 +1,13 @@
+package umc.bean_stalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanStalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanStalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/bean_stalk/BeanStalkApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.bean_stalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanStalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
